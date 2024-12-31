eff86f6e1144fd6020337822328186099f9137ff
Raphael Moreto <raphaelqmoreto@gmail.com>
2024-12-31 19:19:15 -0300

    IMPLEMENTADO BUSCA SIMPLES E AGRUPADA

--- sales-web-mvc/Program.cs
+++ sales-web-mvc/Program.cs
@@ -30,6 +30,7 @@ namespace sales_web_mvc
             builder.Services.AddScoped<SeedingService>();
             builder.Services.AddScoped<SellerService>();
             builder.Services.AddScoped<DepartmentService>();
+            builder.Services.AddScoped<SalesRecordService>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
